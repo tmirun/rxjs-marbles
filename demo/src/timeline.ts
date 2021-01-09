cb4aef1776286e55ccd47f182cd989887a12c5b5
tmirun <tmirun@hotmail.com>
2021-01-09 01:01:48 +0100

    feat: create timeline

---
+++ demo/src/timeline.ts
@@ -0,0 +1,36 @@
+import { interval, merge, Observable, Subject } from 'rxjs';
+import { mapTo, scan, switchMap, takeUntil } from 'rxjs/operators';
+
+export class Timeline {
+  public start$ = new Subject();
+  public reset$ = new Subject();
+  public stop$ = new Subject();
+  public time$: Observable<number>
+
+  constructor() {
+    // ref: https://codepen.io/belfz/pen/WwrBej
+    this.time$ = merge(
+      this.start$.pipe(
+        switchMap(() =>
+          interval(500).pipe(takeUntil(this.stop$))
+        ),
+        mapTo(1)
+      ),
+      this.reset$.pipe(mapTo(0))
+    ).pipe(
+      scan((acc, n) => n === 0 ? 0 : acc + n),
+    )
+  }
+
+  start() {
+    this.start$.next();
+  }
+
+  stop() {
+    this.stop$.next();
+  }
+
+  restart() {
+    this.reset$.next();
+  }
+}
