b654058c75e2c443c08b2ebc2b0003b99e0cc7c1
tmirun <tmirun@hotmail.com>
2021-02-10 01:37:17 +0100

    feat: keep scroll to right

--- demo/src/libs/rx-marbles.ts
+++ demo/src/libs/rx-marbles.ts
@@ -1,5 +1,5 @@
 import { Svg, SVG } from '@svgdotjs/svg.js';
-import { Observable, isObservable, Subject, Subscription, observable } from 'rxjs';
+import { isObservable, Observable, Subscription } from 'rxjs';
 import { Timeline } from './timeline';
 import { RxObservable } from './rx-observable';
 
@@ -11,19 +11,27 @@ export class RxMarbles {
   private currentY = 0;
   private showOperators = false;
   private subscriptions: Subscription = new Subscription();
+  private element: HTMLElement;
 
-  constructor(svgElement: string, source$: Observable<any>) {
-    this.draw = SVG().addTo(svgElement).size(0, 0);
+  constructor(parent: string, source$: Observable<any>) {
+    this.element = document.querySelector(parent) as HTMLElement;
+    this.element.style.width = '100%';
+    this.element.style.overflow = 'auto';
+
+    this.draw = SVG().addTo(this.element).size(0, 0);
 
     this.timeline.time$.pipe().subscribe(
       () => {
         this.refreshSvgSize();
+        this.refreshElementScrollLeft();
       },
       null,
       () => {
         this.timeline.stop();
       }
     );
+
+    const source$1 = source$.pipe();
 
     this.subscribe(source$);
     this.timeline.start();
@@ -61,6 +69,18 @@ export class RxMarbles {
       .animate({ duration: this.timeline.period })
       .width(width + this.padding)
       .height(height + this.padding);
+  }
+
+  refreshElementScrollLeft() {
+    const maxScrollLeft = this.element.scrollWidth - this.element.clientWidth;
+    const offset = 20; // in px
+    const keepScrollToRight =
+      this.element.scrollWidth - offset < this.element.clientWidth + this.element.scrollLeft;
+    if (keepScrollToRight) {
+      this.element.scrollTo({
+        left: maxScrollLeft
+      });
+    }
   }
 
   destroy() {
