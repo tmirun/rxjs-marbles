27df7ab4040fca0fa29725d095472b6987b92f2d
tmirun <tmirun@hotmail.com>
2021-02-14 20:37:45 +0100

    feat: building dot

--- demo/src/libs/dot.ts
+++ demo/src/libs/dot.ts
@@ -2,10 +2,12 @@ import { G, Svg } from '@svgdotjs/svg.js';
 import tippy from 'tippy.js';
 import { RxAxisType } from './rx-axis';
 import 'tippy.js/dist/tippy.css';
+import { isObservable } from 'rxjs';
 
 interface Options {
-  value: string;
+  value: any;
   cy: number;
+  cx: number;
   color: string;
   rxAxisType: RxAxisType;
 }
@@ -15,45 +17,46 @@ export const RX_DOT_RADIUS = RX_DOT_SIZE / 2;
 export const RX_DOT_STROKE = 4;
 export const RX_DOT_RADIUS_OUTER = RX_DOT_RADIUS + RX_DOT_STROKE;
 
-export function drawDot(draw: Svg | G, { value, cy, color, rxAxisType }: Options) {
-  const originalValue = value.toString();
-  const text = originalValue.length > 3 ? '...' : originalValue;
+export function drawDot(draw: Svg | G, { value, cx, cy, color }: Options) {
+  let label = '';
 
   const dot = draw.group();
 
-  const dashLine = dot.line(0, 0, 0, 0).stroke({ dasharray: '5', color: '#000' });
+  if (isObservable(value)) {
+    dot
+      .circle()
+      .center(0, 0)
+      .fill('rgba(255,255,255, 0.5)')
+      .animate()
+      .size(RX_DOT_SIZE, RX_DOT_SIZE);
+    label = 'new observable';
+  } else {
+    label = value.toString();
+    const text = label.length > 3 ? '...' : label;
+    dot
+      .circle(0)
+      .center(0, 0)
+      .fill('rgba(255, 255, 255, 0.8)')
+      .stroke({ color: color, width: RX_DOT_STROKE })
+      .animate()
+      .size(RX_DOT_SIZE, RX_DOT_SIZE);
 
-  switch (rxAxisType) {
-    case 'start':
-      dashLine.attr({ y1: cy, y2: cy });
-      break;
-    case 'middle':
-      dashLine.attr({ y1: 0, y2: cy });
-      break;
-    case 'final':
-      dashLine.attr({ y1: 0, y2: cy });
-      break;
+    dot.text(text).center(0, 0).attr({ fill: '#000' }).attr({
+      style: 'user-select: none; cursor: default; outline: none'
+    });
   }
 
-  const circleGroup = dot.group();
-  circleGroup
-    .circle(0)
-    .center(0, cy)
-    .fill('#fff')
-    .stroke({ color: color, width: RX_DOT_STROKE })
-    .animate()
-    .size(RX_DOT_SIZE, RX_DOT_SIZE);
-
-  circleGroup.text(text).center(0, cy).attr({ fill: '#000' }).attr({
-    style: 'user-select: none; cursor: default; outline: none'
-  });
-
-  tippy([circleGroup.node], {
-    content: originalValue,
+  tippy([dot.node], {
+    content: label,
     placement: 'top'
   });
 
   dot.opacity(0).animate().attr({ opacity: 1 });
+
+  dot.transform({
+    translateX: cx,
+    translateY: cy
+  });
 
   return dot;
 }
