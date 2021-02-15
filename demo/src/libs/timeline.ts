2051e71be7dc053ac3782f8bb8a5e0ac9bcb5eca
tmirun <tmirun@hotmail.com>
2021-02-15 01:03:31 +0100

    fix : add share() in timeline to execute only 1 time the operators

--- demo/src/libs/timeline.ts
+++ demo/src/libs/timeline.ts
@@ -1,37 +1,33 @@
 import { interval, Observable, ReplaySubject, Subject } from 'rxjs';
-import { switchMap, takeUntil, tap } from 'rxjs/operators';
-import * as SVG from '@svgdotjs/svg.js';
+import { share, switchMap, takeUntil, tap } from 'rxjs/operators';
 
 export class Timeline {
-  public period = 100; // millisecond
+  public period = 1000; // millisecond
   public start$ = new ReplaySubject(1);
   public stop$ = new Subject();
   public time$: Observable<number>;
   public counter = 0;
-  public timeSpace = 1;
-  public svgTimeline = new SVG.Timeline();
+  public timeSpace = 10;
 
   constructor() {
     // ref: https://codepen.io/belfz/pen/WwrBej
     this.time$ = this.start$.pipe(
       switchMap(() => interval(this.period).pipe(takeUntil(this.stop$))),
-      tap(() => this.counter++)
+      tap(() => this.counter++),
+      share()
     );
   }
 
   start() {
     this.start$.next();
-    this.svgTimeline.play();
   }
 
   stop() {
     this.stop$.next();
-    this.svgTimeline.stop();
   }
 
   finish() {
     this.start$.complete();
-    this.svgTimeline.finish();
   }
 
   getTimeSpace(offset: number = 0) {
